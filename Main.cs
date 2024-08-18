093d77ceee164f4e2ecf982e8a7a6f05907ba299
Jeremy Pople <jpople@gmail.com>
2024-08-18 13:00:35 -0500

    merge upstream

--- Main.cs
+++ Main.cs
@@ -2,53 +2,53 @@ using System;
 using Godot;
 
 public partial class Main : Node2D {
-	Texture2D brickTexture = GD.Load<Texture2D>("res://sprites/tile_brick.png");
-	Texture2D sunTexture = GD.Load<Texture2D>("res://sprites/tile_sun.png");
+    Texture2D brickTexture = GD.Load<Texture2D>("res://sprites/tile_brick.png");
+    Texture2D sunTexture = GD.Load<Texture2D>("res://sprites/tile_sun.png");
 
-	GridController controller;
+    GridController controller;
 
-	public override void _Ready() {
-		GetWindow().Size = new Vector2I() {
-			X = GridData.DEFAULT_GRID_WIDTH * GridData.CELL_WIDTH,
-			Y = GridData.DEFAULT_GRID_HEIGHT * GridData.CELL_HEIGHT,
-		};
+    public override void _Ready() {
+        GetWindow().Size = new Vector2I() {
+            X = GridData.DEFAULT_GRID_WIDTH * GridData.CELL_WIDTH,
+            Y = GridData.DEFAULT_GRID_HEIGHT * GridData.CELL_HEIGHT,
+        };
 
-		controller = new GridController();
-		controller.createGrid();
-	}
+        controller = new GridController();
+        controller.createGrid();
+    }
 
-	public override void _Draw() {
-		foreach (GridCell cell in controller.GetCells()) {
-			var texture = cell.getContents() switch {
-				GridCellContents.Block => brickTexture,
-				GridCellContents.Jewel => sunTexture,
-				_ => null
-			};
-			if (texture == null) {
-				continue;
-			}
-			var position = new Vector2() {
-				X = cell.getColumn() * GridData.CELL_WIDTH,
-				Y = cell.getRow() * GridData.CELL_HEIGHT,
-			};
-			DrawTexture(texture, position);
-		}
-	}
+    public override void _Draw() {
+        foreach (GridCell cell in controller.GetCells()) {
+            var texture = cell.getContents() switch {
+                GridCellContents.Block => brickTexture,
+                GridCellContents.Jewel => sunTexture,
+                _ => null
+            };
+            if (texture == null) {
+                continue;
+            }
+            var position = new Vector2() {
+                X = cell.getColumn() * GridData.CELL_WIDTH,
+                Y = cell.getRow() * GridData.CELL_HEIGHT,
+            };
+            DrawTexture(texture, position);
+        }
+    }
 
-	private void Advance() {
-		controller.moveBlocks();
-		if (GD.Randf() < 0.2f) {
-			var randomColumn = (int)GD.Randi() % GridData.DEFAULT_GRID_WIDTH;
-			var randomBlockType = (int)(GD.Randi() % Enum.GetValues<BlockType>().Length);
-			controller.addBlock(randomColumn, (BlockType)randomBlockType);
-		}
-		QueueRedraw();
-	}
+    private void Advance() {
+        controller.moveBlocks();
+        if (GD.Randf() < 0.2f) {
+            var randomColumn = (int)(GD.Randi() % GridData.DEFAULT_GRID_WIDTH);
+            var randomBlockType = (int)(GD.Randi() % Enum.GetValues<BlockType>().Length);
+            controller.addBlock(randomColumn, (BlockType)randomBlockType);
+        }
+        QueueRedraw();
+    }
 
-	public override void _Input(InputEvent @event) {
-		if (@event is not InputEventKey e || e.Keycode != Key.Space || !e.Pressed) {
-			return;
-		}
-		Advance();
-	}
+    public override void _Input(InputEvent @event) {
+        if (@event is not InputEventKey e || e.Keycode != Key.Space || !e.Pressed) {
+            return;
+        }
+        Advance();
+    }
 }
